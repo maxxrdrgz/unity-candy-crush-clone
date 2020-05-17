c0eee19a6e42b988403c493248cd6a4dc9cb664d
Maxx Rodriguez <maxxrdrgz@gmail.com>
2020-05-17 16:42:00 -0700

    setup candy script for candy gameobject

---
+++ Assets/Scripts/Candy Scripts/Candy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+
+    public BonusType Bonus {get; set;}
+    public int Row {get; set;}
+    public int Col {get; set;}
+
+    public string Type {get; set;}
+
+    public Candy(){
+        Bonus = BonusType.None;
+    }
+
+    public bool IsSameType(Candy otherCandy){
+        return string.Compare(this.Type, otherCandy.type) == 0;
+    }
+
+    public void Initialize(string type, int row, int col){
+        Col = col;
+        Row = row;
+        Type = type;
+    }
+
+    public static void SwapRowCol(Candy c1, Candy c2){
+        int temp = c1.Row;
+        c1.Row = c2.Row;
+        c2.Row = temp;
+
+        temp = c1.Col;
+        c1.Col = c2.Col;
+        c2.Col = temp;
+    }
+
+}
